9920c40c671b69a7e0bd24402b7ea156573c2402
Lucas Meneghelli Pereira <lucas-m.p@hotmail.com>
2020-11-26 00:04:12 -0300

    - Reworked directories to test case; - Started polling implementation.

--- 04-HelloRPC/src/hw_client.c
+++ 04-HelloRPC/src/hw_client.c
@@ -23,36 +23,45 @@ int main (int argc, char *argv[]) {
 		exit(1);
 	}
 
-	// Conexão com servidor RPC
-	cl = clnt_create(argv[1], PROG, VERS, "tcp");
-	if (cl == NULL) {
-		clnt_pcreateerror(argv[1]);
-		exit(1);
+	if(strcmp(argv[3], "poll") != 0){ // se for envio de arquivo
+
+		// Conexão com servidor RPC
+		cl = clnt_create(argv[1], PROG, VERS, "tcp");
+		if (cl == NULL) {
+			clnt_pcreateerror(argv[1]);
+			exit(1);
+		}
+
+		// Atribuições de valores para os parâmetros
+		strcpy (par_f4.nickname, argv[2]);
+		FILE *fileread;
+		char *msg = NULL;
+		fileread = fopen(argv[3],"r");
+		if (fileread == NULL) {
+			printf("Error: Na abertura dos arquivos (%s).", argv[3]);
+			exit(1);
+		}
+		do{
+			msg = readline(fileread);
+			strcat (par_f4.content, msg);
+		}while(strlen(msg) == 256);
+		fclose(fileread);	
+
+		// Chamadas das funções remotas
+		printf ("\nEnviando mensagem de %s...\n", par_f4.nickname);
+		ret_f4 = func4_1(&par_f4, cl);
+		if (ret_f4 == NULL) {
+			clnt_perror(cl,argv[1]);
+			exit(1);
+		}
+		printf ("Enviado com sucesso!\n");
+	
+	}else{ // se for polling
+
+		
+
 	}
 
-	// Atribuições de valores para os parâmetros
-	strcpy (par_f4.nickname, argv[2]);
-	FILE *fileread;
-	char *msg = NULL;
-	fileread = fopen(argv[3],"r");
-	if (fileread == NULL) {
-		printf("Error: Na abertura dos arquivos (%s).", argv[3]);
-		exit(1);
-	}
-	do{
-		msg = readline(fileread);
-		strcat (par_f4.content, msg);
-	}while(strlen(msg) == 256);
-	fclose(fileread);	
-
-	// Chamadas das funções remotas
-	printf ("\nEnviando mensagem de %s...\n", par_f4.nickname);
-	ret_f4 = func4_1(&par_f4, cl);
-	if (ret_f4 == NULL) {
-	    clnt_perror(cl,argv[1]);
-	    exit(1);
-	}
-	printf ("Enviado com sucesso!\n");
 
 	return 0;
 }
